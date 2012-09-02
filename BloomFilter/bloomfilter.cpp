d57ca0c6feff83712b45fe5823afb74280954fac
Yohei Yamaguchi <joker13meister@gmail.com>
2012-09-02 22:42:25 +0900

    added comment

--- BloomFilter/bloomfilter.cpp
+++ BloomFilter/bloomfilter.cpp
@@ -3,17 +3,34 @@
 #include <bitset>
 using namespace std;
 
+// ビット列の長さ
 static const int kNumBits                   = 1 << 10;
+// ハッシュ関数の個数
 static const int kNumHashFuncs              = 3;
+// ハッシュ関数の salt
 static const unsigned kSalts[kNumHashFuncs] = {137, 69, 545};
 
+/**
+ * Bloom Filter
+ */
 class BloomFilter
 {
     bitset<kNumBits> _bs;
     vector<unsigned> genHashes(const string &key);
 public:
+    /**
+     * constructor
+     */
     BloomFilter();
+
+    /**
+     * 要素を追加する
+     */
     void add(const string &key);
+
+    /**
+     * 要素の存在をチェックする
+     */
     bool exist(const string &key);
 };
 
